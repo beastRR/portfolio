bc797058e34d6e7b1ef6487d0dbb35a7ab5f2277
beastRR <89104224+beastRR@users.noreply.github.com>
2022-09-19 00:34:00 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ const About =()=>{
             <div className=" card1">
                 <div className="card2">
                     <div className="row">
-                        <div className="col-8 mb-5">
+                        <div className="col-11 mb-5">
                         <h1 className="text-warning">Rahul Raushan</h1>
                         <p className="paraAbout"> I am a Web Developer, I like the FrontEnd part more. I have a keen interest in developing web applications.
                         I enjoy it a lot. I have completed my Masters Of Computer Application (MCA) this year 2022.
@@ -19,10 +19,10 @@ const About =()=>{
                         when I start working on a new technology.   </p>
                         </div>
 
-                        <div className="col-4">
+                        <div className="col-1">
 
                         </div>
-                        <div className="col-8">
+                        <div className="col-11 pb-5">
                             <h3 className="text-success" style={{fontWeight:"bolder"}}>
                                 <span style={{color:"rgb(130, 40, 221)"}}>|&nbsp;</span>
                                 Technologies</h3>
@@ -31,7 +31,7 @@ const About =()=>{
                                 so, I am also a MERN Developer.
                             </p>
                         </div>
-                        <div className="col-4"></div>
+                        <div className="col-1"></div>
                     </div>
                     
             </div>
